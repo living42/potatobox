5e018e6285e923816eba0ded3adaadbe37f0f87f
living42 <qzeqing@gmail.com>
2020-08-29 16:56:55 +0800

    ability to setup consul client

--- deploy/image/main/scripts/setup-consul.sh
+++ deploy/image/main/scripts/setup-consul.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 set -xeu
 
-TYPE=$1
-CONSUL_DATA_DIR=$2
-SERVER_TAGS=$3
-
-[ -d "$CONSUL_DATA_DIR" ] || {
-    mkdir $CONSUL_DATA_DIR
-    chown consul:consul $CONSUL_DATA_DIR
-}
-
-CONSUL_SERVER_ADDRS=$(env HOME=/root aliyun ecs DescribeInstances \
-    $SERVER_TAGS \
-    | jq '[.Instances.Instance[].VpcAttributes.PrivateIpAddress.IpAddress[0]]')
-
 REGION=$(curl -sS 100.100.100.200/2016-01-01/meta-data/region-id)
 
-BOOTSTRAP_EXPECT=$(echo $CONSUL_SERVER_ADDRS | jq length)
+setup_server () {
+  CONSUL_DATA_DIR=$1
+  SERVER_TAGS=$2
 
-cat <<EOF > /etc/consul.d/consul.hcl
+  [ -d "$CONSUL_DATA_DIR" ] || {
+      mkdir $CONSUL_DATA_DIR
+      chown consul:consul $CONSUL_DATA_DIR
+  }
+
+  CONSUL_SERVER_ADDRS=$(env HOME=/root aliyun ecs DescribeInstances \
+      $SERVER_TAGS \
+      | jq '[.Instances.Instance[].VpcAttributes.PrivateIpAddress.IpAddress[0]]')
+
+  BOOTSTRAP_EXPECT=$(echo $CONSUL_SERVER_ADDRS | jq length)
+
+  cat <<EOF > /etc/consul.d/consul.hcl
 datacenter = "${REGION}"
 data_dir = "${CONSUL_DATA_DIR}"
 client_addr = "127.0.0.1 {{ GetInterfaceIP \"eth0\" }}"
@@ -32,11 +32,55 @@ bind_addr = "{{ GetInterfaceIP \"eth0\" }}"
 retry_join = ${CONSUL_SERVER_ADDRS}
 EOF
 
-chown consul:consul /etc/consul.d/consul.hcl
-chmod 600 /etc/consul.d/consul.hcl
+  chown consul:consul /etc/consul.d/consul.hcl
+  chmod 600 /etc/consul.d/consul.hcl
+
+  setup_systemd_service
+
+  check_deadline=$(($(date +%s) + 60))
+  until [ "$(curl -sS localhost:8500/v1/status/peers | jq length)" = "$BOOTSTRAP_EXPECT" ]; do
+    if [ "$(date +%s)" -ge "$check_deadline" ]; then
+      echo "consul did not up"
+      exit 1
+    fi
+    echo "waiting consul servers up"
+    sleep 1
+  done
+}
+
+setup_client () {
+  CONSUL_SERVER_ADDRS=$1
+
+  if [ -d /data ]; then
+    CONSUL_DATA_DIR=/data/consul/client
+  else
+    CONSUL_DATA_DIR=/var/lib/consul/client
+  fi
+
+  mkdir -p $CONSUL_DATA_DIR
+  chown -R consul:consul $CONSUL_DATA_DIR
+
+  cat <<EOF > /etc/consul.d/consul.hcl
+datacenter = "${REGION}"
+data_dir = "${CONSUL_DATA_DIR}"
+client_addr = "127.0.0.1 {{ GetInterfaceIP \"eth0\" }}"
+ports = {
+  "dns" = 53
+}
+server = false
+bind_addr = "{{ GetInterfaceIP \"eth0\" }}"
+retry_join = ${CONSUL_SERVER_ADDRS}
+EOF
+
+  chown consul:consul /etc/consul.d/consul.hcl
+  chmod 600 /etc/consul.d/consul.hcl
+
+  setup_systemd_service
+}
 
 
-cat <<EOF > /etc/systemd/system/consul.service
+setup_systemd_service() {
+  cat <<EOF > /etc/systemd/system/consul.service
 [Unit]
 Description=Consul
 After=network.target
@@ -52,25 +96,16 @@ AmbientCapabilities=CAP_NET_BIND_SERVICE
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-systemctl enable consul
-systemctl start consul
+  systemctl daemon-reload
+  systemctl enable consul
+  systemctl start consul
+}
 
-check_deadline=$(($(date +%s) + 60))
-until [ "$(curl -sS localhost:8500/v1/status/peers | jq length)" = "$BOOTSTRAP_EXPECT" ]; do
-  if [ "$(date +%s)" -ge "$check_deadline" ]; then
-    echo "consul did not up"
-    exit 1
-  fi
-  echo "waiting consul service up"
-  sleep 1
-done
-
-
-# Modify resolv.conf to forward dns lookup into consul, so we can use hostname
-# to communicate each other, further use service name to
-# NOTE this only tested on Debian 10, may no work on other distro
-cat <<EOF >> /etc/resolvconf/resolv.conf.d/head
+setup_dns_server() {
+  # Modify resolv.conf to forward dns lookup into consul, so we can use hostname
+  # to communicate each other, further use service name to
+  # NOTE this only tested on Debian 10, may no work on other distro
+  cat <<EOF >> /etc/resolvconf/resolv.conf.d/head
 search node.consul service.consul
 nameserver 127.0.0.1
 EOF
@@ -79,4 +114,22 @@ cat <<EOF > /etc/resolvconf/resolv.conf.d/tail
 options timeout:2 attempts:3
 EOF
 
-systemctl restart networking
+  systemctl restart networking
+}
+
+TYPE=$1
+
+case $TYPE in
+  server)
+    setup_server "$2" "$3"
+    setup_dns_server
+  ;;
+  client)
+    setup_client "$2"
+    setup_dns_server
+  ;;
+  *)
+    echo "invalid TYPE $TYPE"
+    exit 1
+  ;;
+esac
